42203e074713d5ea4ca263a7f6510aff492abb62
MichalOkon <michalionis123@gmail.com>
2023-04-05 15:28:41 +0200

    Create basic precision tests

---
+++ tests/losses/test_precision.py
@@ -0,0 +1,33 @@
+import torch
+from allrank.models.metrics import precision
+def precision_wrap(y_pred, y_true, ats):
+    return precision(torch.tensor([y_pred]), torch.tensor([y_true]), ats)
+def test_precision_simple_1():
+    y_pred = [0.5, 0.2]
+    y_true = [1.0, 0.0]
+    ats = [1]
+
+    result = precision_wrap(y_pred, y_true, ats)
+    expected = 1.0
+
+    assert result == expected
+
+def test_precision_simple_2():
+    y_pred = [0.5, 0.2]
+    y_true = [0.0, 1.0]
+    ats = [1]
+
+    result = precision_wrap(y_pred, y_true, ats)
+    expected = 0.0
+
+    assert result == expected
+
+def test_precision_simple_3():
+    y_pred = [0.5, 0.2]
+    y_true = [0.0, 1.0]
+    ats = [2]
+
+    result = precision_wrap(y_pred, y_true, ats)
+    expected = 0.5
+
+    assert result == expected
