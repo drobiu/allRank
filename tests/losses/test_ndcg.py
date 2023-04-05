596c11b820966531b3ae1d49890ca6954e1ece34
MichalOkon <michalionis123@gmail.com>
2023-04-05 21:36:20 +0200

    Add ndcg and precision tests

--- tests/losses/test_ndcg.py
+++ tests/losses/test_ndcg.py
@@ -6,11 +6,13 @@ from pytest import approx
 from allrank.data.dataset_loading import PADDED_Y_VALUE
 from allrank.models.metrics import ndcg
 
+import numpy as np
 
 def ndcg_wrap(y_pred, y_true, ats=None):
     return ndcg(torch.tensor([y_pred]), torch.tensor([y_true]), ats=ats).numpy()
 
-
+def ndcg_wrap_multiple_slates(y_pred, y_true, ats=None):
+    return ndcg(torch.tensor(y_pred), torch.tensor(y_true), ats=ats).numpy()
 def test_ndcg_simple_1():
     y_pred = [0.5, 0.2]
     y_true = [1.0, 0.0]
@@ -29,13 +31,13 @@ def test_ndcg_simple_2():
     assert (result == 1 / math.log2(3))
 
 
-def test_ndcg_zero_when_no_relevant():
+def test_ndcg_one_when_no_relevant():
     y_pred = [0.5, 0.2]
     y_true = [0.0, 0.0]
 
     result = ndcg_wrap(y_pred, y_true)
 
-    assert (result == 0.0)
+    assert (result == 1.0)
 
 
 def test_ndcg_for_multiple_ats():
@@ -67,3 +69,16 @@ def test_ndcg_with_padded_input_2():
     result = ndcg_wrap(y_pred, y_true)
 
     assert result == 1 / math.log2(3)
+
+def test_baseline_ndcg():
+    predictions = [[0.9, 0.85, 0.71, 0.63],
+                   [0.87, 0.76, 0.64, 0.26],
+                   [0.7, 0.65, 0.32, 0.1]]
+
+    ground_truth = [[2.0, 0.0, 2.0, 1.0],
+                    [0.0, 2.0, 1.0, 0.0],
+                    [1.0, 1.0, 0.0, 0.0]]
+
+    result = ndcg_wrap_multiple_slates(predictions, ground_truth, ats=[4])
+
+    np.testing.assert_almost_equal(result.mean(), 0.857, decimal=3)
--- tests/losses/test_precision.py
+++ tests/losses/test_precision.py
@@ -1,8 +1,11 @@
 import torch
 from allrank.models.metrics import precision
+import numpy as np
 def precision_wrap(y_pred, y_true, ats):
     return precision(torch.tensor([y_pred]), torch.tensor([y_true]), ats)
 
+def precision_wrap_multiple_slates(y_pred, y_true, ats, cutoff = 1):
+    return precision(torch.tensor(y_pred), torch.tensor(y_true), ats, cutoff=cutoff)
 def test_precision_simple_1():
     y_pred = [0.5, 0.2]
     y_true = [2.0, 0.0]
@@ -32,6 +35,21 @@ def test_precision_simple_3():
     expected = 0.5
 
     assert result == expected
+
+def test_baseline_precision():
+    predictions = [[0.9, 0.85, 0.71, 0.63],
+                   [0.87, 0.76, 0.64, 0.26],
+                   [0.7, 0.65, 0.32, 0.1]]
+
+    ground_truth = [[2.0, 0.0, 2.0, 1.0],
+                    [0.0, 2.0, 1.0, 0.0],
+                    [1.0, 1.0, 0.0, 0.0]]
+
+    result = precision_wrap_multiple_slates(predictions, ground_truth, ats=[1, 4], cutoff=2)
+
+    np.testing.assert_almost_equal(result[:,0].mean(), 0.333, decimal=3)
+    np.testing.assert_almost_equal(result[:,1].mean(), 0.250, decimal=3)
+
 
 if __name__ == '__main__':
     test_precision_simple_1()
