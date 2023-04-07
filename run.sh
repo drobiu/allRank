b87c99f3d81ad2ba77eb01760c32ac2d9420c49e
Karol Dobiczek <k.t.dobiczek@student.tudelft.nl>
2023-04-07 02:30:54 +0200

    fix metrics for experiments

--- run.sh
+++ run.sh
@@ -1,7 +1,10 @@
-for i in {1..5}
+for arch in deep shallow
     do
-        for LOSS in neuralNDCG rankNet rmse
+        for i in {1..5}
             do
-                python allrank/main.py --config-file-name configs/allRank/deep/${LOSS}_config_MQ.json --run-id deep_${LOSS}_MQ_${i} --job-dir test_logs/deep_${LOSS}_MQ/fold_${i} --fold-dir Fold${i}
+                for LOSS in neuralNDCG rankNet rmse
+                    do
+                        python allrank/main.py --config-file-name configs/allRank/${arch}/${LOSS}_config_MQ.json --run-id ${arch}_${LOSS}_MQ_${i} --job-dir test_logs/MQ/${arch}_${LOSS}/fold_${i} --fold-dir Fold${i}
+                    done
             done
     done
--- run_mslr.sh
+++ run_mslr.sh
@@ -1,7 +1,7 @@
-for i in {1..5}
+for arch in deep shallow
     do
         for LOSS in approxNDCG neuralNDCG rankNet rmse
             do
-                python allrank/main.py --config-file-name configs/allRank/deep/${LOSS}_config_MSLR.json --run-id deep_${LOSS}_MSLR_${i} --job-dir test_logs/deep_${LOSS}_MSLR/fold_${i} --fold-dir Fold${i}
+                python allrank/main.py --config-file-name configs/allRank/${arch}/${LOSS}_config_MSLR.json --run-id ${arch}_${LOSS}_MSLR_1 --job-dir test_logs/MSLR/${arch}_${LOSS}/fold_1 --fold-dir Fold1
             done
     done
