5ea2b46bebd9199e1eace24829a77c33afaa5f3a
Karol Dobiczek <k.t.dobiczek@student.tudelft.nl>
2023-04-07 23:22:29 +0200

    final configs

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@ for arch in deep shallow
     do
         for i in {1..5}
             do
-                for LOSS in neuralNDCG rankNet rmse
+                for LOSS in rmse, listMLE, approxNDCG, neuralNDCG, rankNet
                     do
                         python allrank/main.py --config-file-name configs/allRank/${arch}/${LOSS}_config_MQ.json --run-id ${arch}_${LOSS}_MQ_${i} --job-dir test_logs/MQ/${arch}_${LOSS}/fold_${i} --fold-dir Fold${i}
                     done
